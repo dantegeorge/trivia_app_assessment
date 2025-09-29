1bf446abdb6f7d23d49f569c0e7e5284e3746771
Dante <dantesamuelgeorge@gmail.com>
2025-09-29 11:36:55 +0200

    pushed trivia app base

---
+++ src/main/java/com/example/trivia_app_assessment/TriviaAppAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.trivia_app_assessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaAppAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaAppAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trivia_app_assessment/TriviaAppAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trivia_app_assessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaAppAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
